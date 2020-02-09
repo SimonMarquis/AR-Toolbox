565be5bdae68fa1241b511a48c898f39b7037419
Simon Marquis <contact@simon-marquis.fr>
2020-02-09 11:14:30 +0100

    Refactor SharedPreference-based MutableLiveData

--- app/src/main/java/fr/smarquis/ar_toolbox/SceneViewModel.kt
+++ app/src/main/java/fr/smarquis/ar_toolbox/SceneViewModel.kt
@@ -8,33 +8,20 @@ import kotlin.reflect.KClass
 
 class SceneViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val prefs = PreferenceManager.getDefaultSharedPreferences(application)
-
     val selection = MutableLiveData<KClass<out Nodes>>(Sphere::class)
 
     val externalModelUri: MutableLiveData<String?> = object : MutableLiveData<String?>() {
 
-        val KEY_URI = "KEY_URI"
+        private val prefs = PreferenceManager.getDefaultSharedPreferences(application)
+
+        private val KEY_URI = "KEY_URI"
 
         init {
             value = prefs.getString(KEY_URI, null)
         }
 
-        override fun postValue(value: String?) {
-            value.let {
-                updateValue(it)
-                super.postValue(it)
-            }
-        }
-
         override fun setValue(value: String?) {
-            value.let {
-                updateValue(it)
-                super.setValue(it)
-            }
-        }
-
-        private fun updateValue(value: String?) {
+            super.setValue(value)
             prefs.edit().putString(KEY_URI, value).apply()
         }
 
